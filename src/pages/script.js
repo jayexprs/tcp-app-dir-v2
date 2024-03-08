cefbcd8c9a0190339a54da3841611cf7343cc2b2
Jay McCormick <jayexprs@gmail.com>
2024-03-08 10:17:09 -0500

    product, app card, data, layout

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,6 +1,6 @@
 const appCardGrid = document.getElementById('app-card-grid');
 const appCard = document.getElementById('app-card');
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 5; i++) {
   appCardGrid.appendChild(appCard.cloneNode(true));
 }
